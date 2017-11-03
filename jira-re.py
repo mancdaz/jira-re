66c220dd17f9a43153e16f7718e989db98b6c68f
Darren Birkett <darren.birkett@gmail.com>
2017-11-03 13:32:53 +0000

    add debug option to print issue summary for each section

--- jira-re.py
+++ jira-re.py
@@ -10,6 +10,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-r', '--release', action='store', dest='release')
 parser.add_argument('-pd', '--plan-date', action='store', dest='plan')
 parser.add_argument('-p', '--project', action='store', dest='project')
+parser.add_argument('-d', '--debug', action='store_true', dest='debug')
+
 args = parser.parse_args()
 
 
@@ -18,23 +20,37 @@ PASS = '<your_jira_password>'
 PLAN_DATE = args.plan if args.plan else '2017-10-03'
 CURRENT_RELEASE = args.release if args.release else 'RE-2017.11'
 PROJECT = args.project if args.project else 'RE'
+DEBUG = args.debug
 
-jira = JIRA('https://rpc-openstack.atlassian.net', basic_auth=(USER,PASS))
 
 # check if fixversion actually exists
-fixversions = [version.name for version in jira.project_versions(PROJECT)]
-if CURRENT_RELEASE not in fixversions:
-    print 'Warning: Release %s was not found' % CURRENT_RELEASE
-    exit(1)
+def check_fixversion_exists(project, fixversion):
+    fixversions = [version.name for version in jira.project_versions(project)]
+    if fixversion not in fixversions:
+        print 'Warning: Release %s was not found in project %s ' % (CURRENT_RELEASE, PROJECT)
+        exit(1)
 
-initial_items = jira.search_issues('fixVersion = %s and created <= 2017-10-03 and type in (bug,task)' % CURRENT_RELEASE)
-addl_items = jira.search_issues('fixVersion = %s and created > 2017-10-03 and type in (bug,task)' % CURRENT_RELEASE)
+def print_issue_summary(issue):
+    print issue.key + '\t: ' + issue.fields.summary
+
+def print_issues_summary(issues):
+    for issue in issues:
+        print_issue_summary(issue)
+    print
+
+
+# main
+jira = JIRA('https://rpc-openstack.atlassian.net', basic_auth=(USER,PASS))
+print 'Querying project %s for issues in release %s...' % (PROJECT, CURRENT_RELEASE)
+
+check_fixversion_exists(PROJECT, CURRENT_RELEASE)
+
+initial_items = jira.search_issues('fixVersion = %s and created <= %s and type in (bug,task)' % (CURRENT_RELEASE, PLAN_DATE))
+addl_items = jira.search_issues('fixVersion = %s and created > %s and type in (bug,task)' % (CURRENT_RELEASE, PLAN_DATE))
 total_items_in_release = initial_items + addl_items
-
 completed_items = jira.search_issues('fixVersion = %s and type in (bug,task) and status = Finished' % CURRENT_RELEASE)
 remaining_items = jira.search_issues('fixVersion = %s and type in (bug,task) and status != Finished' % CURRENT_RELEASE)
-
-non_release_items = jira.search_issues('labels in (re-related) AND resolved >= 2017-10-01 and Status in (Finished, Done) ORDER BY created ASC')
+non_release_items = jira.search_issues('labels in (re-related) AND resolved >= %s and Status in (Finished, Done) ORDER BY created ASC' % PLAN_DATE)
 
 remaining_statuses = [a.fields.status.name for a in remaining_items]
 status_count = Counter(remaining_statuses)
@@ -46,12 +62,17 @@ print 'Current Release: %s' % CURRENT_RELEASE
 print 'Total items in release:\t\t\t\t\t %s'  % len(total_items_in_release)
 print
 print 'Initial release items in planning:\t\t\t %s' % len(initial_items)
+if DEBUG: print_issues_summary(initial_items)
 print 'Additional release items since planning:\t\t %s' % len(addl_items)
+if DEBUG: print_issues_summary(addl_items)
 print
 print 'Completed release items :\t\t\t\t %s'  % len(completed_items)
+if DEBUG: print_issues_summary(completed_items)
 print 'Remaining release items :\t\t\t\t %s (%s)'  % (len(remaining_items), status_string)
+if DEBUG: print_issues_summary(remaining_items)
 print
 print 'Completed non-release items:\t\t\t\t %s' % len(non_release_items)
+if DEBUG: print_issues_summary(non_release_items)
 print
 print 'Total items completed in release period:\t\t %s' % (len(completed_items) + len(non_release_items))
 
