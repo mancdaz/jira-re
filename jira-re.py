576b23de71b4e57156f7a5a89453d064cc7cc28f
Darren Birkett <darren.birkett@gmail.com>
2018-10-03 10:25:49 +0100

    python3 and flake8 fixes

--- jira-re.py
+++ jira-re.py
@@ -55,8 +55,8 @@ def get_plan_date():
 def check_fixversion_exists(project, fixversion):
     fixversions = [version.name for version in jira.project_versions(project)]
     if fixversion not in fixversions:
-        print 'Warning: Release %s was not found in project %s ' \
-            % (CURRENT_RELEASE, PROJECT)
+        print('Warning: Release %s was not found in project %s '
+              % (CURRENT_RELEASE, PROJECT))
         exit(1)
 
 
@@ -95,7 +95,7 @@ def print_issues_summary(issues):
                    issue.key,
                    epic_link,
                    summary])
-    print t
+    print(t)
 
 
 def open_link(link):
@@ -118,42 +118,43 @@ def ppp_report():
     print('  - %s additional non-release items completed '
           '(re-related or non-release themed bugs)'
           % len(non_release_items))
-#    open_link('https://rpc-openstack.atlassian.net/issues/?filter=14161')
-    last_seven_days = jira.search_issues(
-        '(project = re OR labels = re-related) '
-        'AND type in (bug, task, sub-task) '
-        'AND resolutiondate >= -7d '
-        'AND resolution not in ("Won\'t Fix", "Won\'t Do", Duplicate) '
-        'ORDER BY resolutiondate ASC'
-    )
+    if DEBUG:
+        open_link('https://rpc-openstack.atlassian.net/issues/?filter=14161')
+    last_seven_days = jira.search_issues('filter=14161')
+#        '(project = re OR labels = re-related) '
+#        'AND type in (bug, task, sub-task) '
+#        'AND resolutiondate >= -7d '
+#        'AND resolution not in ("Won\'t Fix", "Won\'t Do", Duplicate) '
+#        'ORDER BY resolutiondate ASC'
+#   )
     total = len(last_seven_days)
     print('\n%d issues completed in the last seven days:\n' % total)
     print_issues_summary(last_seven_days)
 
 
 def normal_report():
-    print 'Date: %s-%s-%s' % get_date()
-    print 'Planning Date: %s' % PLAN_DATE
-    print 'Current Release: %s' % CURRENT_RELEASE
-    print 'Total (non-epic) items in release:\t\t %s' \
-          % len(total_items_in_release)
+    print('Date: %s-%s-%s' % get_date())
+    print('Planning Date: %s' % PLAN_DATE)
+    print('Current Release: %s' % CURRENT_RELEASE)
+    print('Total (non-epic) items in release:\t\t %s'
+          % len(total_items_in_release))
     print
-    print 'Initial release items in planning:\t\t %s' % len(initial_items)
+    print('Initial release items in planning:\t\t %s' % len(initial_items))
     if DEBUG: print_issues_summary(initial_items)
-    print 'Additional release items since planning:\t %s' % len(addl_items)
+    print('Additional release items since planning:\t %s' % len(addl_items))
     if DEBUG: print_issues_summary(addl_items)
     print
-    print 'Completed release items:\t\t\t %s' % len(completed_items)
+    print('Completed release items:\t\t\t %s' % len(completed_items))
     if DEBUG: print_issues_summary(completed_items)
-    print 'Remaining release items:\t\t\t %s (%s)' \
-        % (len(remaining_items), status_string)
+    print('Remaining release items:\t\t\t %s (%s)'
+          % (len(remaining_items), status_string))
     if DEBUG: print_issues_summary(remaining_items)
     print
-    print 'Completed non-release items:\t\t\t %s' % len(non_release_items)
+    print('Completed non-release items:\t\t\t %s' % len(non_release_items))
     if DEBUG: print_issues_summary(non_release_items)
     print
-    print 'Total items completed in release period:\t %s' \
-        % (len(completed_items) + len(non_release_items))
+    print('Total items completed in release period:\t %s'
+          % (len(completed_items) + len(non_release_items)))
 
 
 ######################
@@ -176,8 +177,8 @@ if args.issue:
     open_issue(args.issue)
     exit(0)
 
-print 'Querying project %s for issues in release %s...' \
-    % (PROJECT, CURRENT_RELEASE)
+print('Querying project %s for issues in release %s...'
+      % (PROJECT, CURRENT_RELEASE))
 
 check_fixversion_exists(PROJECT, CURRENT_RELEASE)
 
